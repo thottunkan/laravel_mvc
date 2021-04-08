f1561509ada9f1843464fbade070d3181653346c
thottunkan <ambadinarayan98@gmail.com>
2021-04-08 12:58:07 +0530

    add form to the facultypage , set post method ,set action and apply name to all textbox

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,28 +5,34 @@
  
     <div class="container" style="margin-top: 100px;">
         <div class="table">
+            <form action="/facultyread" method="POST">
             <table class="table table-borderless">
                 <tr>
+                    <td></td>
+                    <td class="text-success h3"> Faculty</td>
+                </tr>
+                <tr>
                     <td>Name</td>
-                    <td><input type="text"  class="form-control"></td>
+                    <td><input type="text" name="name" class="form-control"></td>
                 </tr>
                 <tr>
                     <td>Designation</td>
-                    <td><input type="text" class="form-control"></td>
+                    <td><input type="text" name="designation" class="form-control"></td>
                 </tr>
                 <tr>
                     <td>College</td>
-                    <td><input type="text" class="form-control"></td>
+                    <td><input type="text" name="college" class="form-control"></td>
                 </tr>
                 <tr>
                     <td>Contact no</td>
-                    <td><input type="text" class="form-control"></td>
+                    <td><input type="text" name="contactno" class="form-control"></td>
                 </tr>
                 <tr>
-                    <td>Contact no</td>
-                    <td><input type="submit" class="btn "></td>
+                    <td></td>
+                    <td><input type="submit" class="btn form-control text-white btn-success "></td>
                 </tr>
             </table>
+            </form>
         </div>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,4 @@ use Illuminate\Support\Facades\Route;
 Route::get("/", [FacultyController::class,'create']);
 Route::get("/student",[StudentController::class,'create']);
 Route::get("/busadd",[BusController::class,'create']);
+Route::post("facultyread",[FacultyController::class,'store']);
